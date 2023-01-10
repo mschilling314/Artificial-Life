b9b7e0b64063076067819134cb56922ad6ae5cef
mschilling314 <mschilling52019@gmail.com>
2023-01-10 14:20:26 -0600

    HW1 Done

--- generate.py
+++ generate.py
@@ -21,17 +21,27 @@ def makeSquareOfTowers(xo, yo, side, num):
         makeTowerRow(xo, yo + i*side, side, num)
 
 
-pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = height / 2 # so that it starts on the floor
-#pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-
-makeSquareOfTowers(x, y, length, 3)
+def sendCube(nomen, x=0, y=0, side=1, zoff = 0):
+    z = side / 2 + zoff# so that it starts on the floor
+    pyrosim.Send_Cube(name=nomen, pos=[x, y, z], size=[side, side, side])
 
 
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    sendCube("Box", -2, -2)
+    pyrosim.End()
 
-pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    sendCube("Torso", zoff=1)
+    #pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [1, 0, 1])
+    pyrosim.Send_Joint( name = "Torso_Backleg" , parent= "Torso" , child = "Backleg" , type = "revolute", position = [0.5, 0, 1])
+    sendCube("Backleg", x=0.5, zoff=-1)
+    pyrosim.Send_Joint( name = "Torso_Frontleg" , parent= "Torso" , child = "Frontleg" , type = "revolute", position = [-0.5, 0, 1])
+    sendCube("Frontleg", x=-0.5, zoff=-1)
+    pyrosim.End()
+
+
+Create_World()
+Create_Robot()
