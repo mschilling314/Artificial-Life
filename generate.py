e7de6a996ffad5624147bfe913a0551c70688718
mschilling314 <mschilling52019@gmail.com>
2023-01-09 22:00:20 -0600

    Tower Square

--- generate.py
+++ generate.py
@@ -1,7 +1,37 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+def makeTower(x, y, z, length, width, height):
+    for i in range(10):
+        pyrosim.Send_Cube(name="Box"+str(i), pos=[x, y, z], size=[length, width, height])
+        z += height / 2
+        length *= 0.9
+        height *= 0.9
+        width *= 0.9
+        z += height / 2
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+
+def makeTowerRow(xo, yo, lo, num):
+    for i in range(num):
+        makeTower(xo + i*lo, yo, lo/2, lo, lo, lo)
+
+
+
+def makeSquareOfTowers(xo, yo, side, num):
+    for i in range(num):
+        makeTowerRow(xo, yo + i*side, side, num)
+
+
+pyrosim.Start_SDF("boxes.sdf")
+length = 1
+width = 1
+height = 1
+x = 0
+y = 0
+z = height / 2 # so that it starts on the floor
+#pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+
+makeSquareOfTowers(x, y, length, 3)
+
+
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,15 @@
 import pybullet as p
+import pybullet_data
+import generate
 import time
 
+generate
+
 physicsClient = p.connect(p.GUI)
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+planeID = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
