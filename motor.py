8ab8b728785bb4fb27c9b38c33a6b8f2a3b635f5
mschilling314 <mschilling52019@gmail.com>
2023-01-18 16:35:22 -0600

    Neurons Completed

--- motor.py
+++ motor.py
@@ -7,18 +7,6 @@ import pybullet as p
 class MOTOR:
     def __init__(self, jointName) -> None:
         self.jointName = jointName
-        self.Prepare_To_Act()
-
-
-    def Prepare_To_Act(self):
-        self.frequency = c.frequency
-        self.amplitude = c.amplitude
-        self.phaseOffset = c.phaseOffset
-        if self.jointName == "Torso_Frontleg":
-            self.frequency *= 2
-            print("\nstatement hit\n")
-        x = np.linspace(0, 2*c.pi, c.iter)
-        self.motorValues = self.amplitude * np.sin(self.frequency * x + self.phaseOffset)
 
     
     def Set_Value(self, desiredAngle, robot):
